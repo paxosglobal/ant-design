21ccf194e57cba9b98fb922280d670de1e9eb1aa
Sam Oliver <soliver@itbit.com>
2021-04-13 12:13:23 -0400

    Added dist, lib, and es to commit

---
+++ lib/skeleton/Title.d.ts
@@ -0,0 +1,8 @@
+export interface SkeletonTitleProps {
+    prefixCls?: string;
+    className?: string;
+    style?: object;
+    width?: number | string;
+}
+declare const Title: ({ prefixCls, className, width, style }: SkeletonTitleProps) => JSX.Element;
+export default Title;
